0fee416859643c7948d5879309a085f6c8d9dbdf
Franky <frank.jr.619@gmail.com>
2016-10-10 14:45:40 +0700

    commit

--- app/src/main/java/test/pawoon/com/pawoontest/ListTitleActivity.java
+++ app/src/main/java/test/pawoon/com/pawoontest/ListTitleActivity.java
@@ -24,6 +24,7 @@ public class ListTitleActivity extends ListActivity {
                     android.R.layout.simple_list_item_1, listTitle);
 
             setListAdapter(adapter);
+            //
         }
 
     }
